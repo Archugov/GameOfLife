6d620fed84b0e2d2f037e50cea4fb3aa477caba4
Archugov <archugov.pavel@gmail.com>
2022-01-26 17:48:32 +0300

    Ver 0.3

--- GameOfLife/Form1.cs
+++ GameOfLife/Form1.cs
@@ -40,12 +40,8 @@ namespace GameOfLife
             
             Random random = new Random();
             for (int x = 0; x < cols; x++)
-            {
                 for (int y = 0; y < rows; y++)
-                {
                     field[x,y] = random.Next((int)numDensity.Value) == 0;
-                }
-            }
 
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             graphics = Graphics.FromImage(pictureBox1.Image);
@@ -68,18 +64,50 @@ namespace GameOfLife
         {
             graphics.Clear(Color.Black);
 
+            var newField = new bool[cols, rows];
+
             for (int x = 0; x < cols; x++)
             {
                 for (int y = 0; y < rows; y++)
                 {
-                    if (field[x,y])
-                    {
+                    var neighboursCount = fieldNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighboursCount == 3)
+                        newField[x, y] = true;
+                    else if (hasLife && neighboursCount < 2 || neighboursCount > 3)
+                        newField[x, y] = false;
+                    else
+                        newField[x, y] = field[x, y];
+                    if (hasLife)
                         graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution, resolution);
-                    }
                 }
             }
 
+            field = newField;
+
             pictureBox1.Refresh();
+        }
+
+        private int fieldNeighbours(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = x + i;
+                    var row = y + j;
+                    var isSelfCheck = col == x && row == y;
+                    var hasLife = field[col, row];
+
+                    if (hasLife && !isSelfCheck)
+                        count++;
+                }
+            }
+
+            return count;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
